0cbae6bc26b3b3a8e8abf03e857956d75cd87d6c
Hudson Marques <lc.coutinhom@gmail.com>
2019-09-19 17:34:52 -0300

    test git flow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,10 +6,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'Locadora.com.br' }
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
